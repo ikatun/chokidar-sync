ac0156c0142b424e4161750baa10feb8e12765c3
Ivo Katunaric <ivo@shoutem.com>
2017-07-11 22:47:52 +0200

    inital commit

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const path = require('path');
+require('colors');
+const sync = require('./');
+
+const srcDir = process.argv[2];
+const destDir = process.argv[3];
+
+function log(args) {
+  const destPath = path.join(destDir, args.relative);
+  if (args.type === 'add') {
+    console.log(destPath.green);
+  } else if (args.type === 'change') {
+    console.log(destPath);
+  } else {
+    console.log(destPath.red);
+  }
+}
+
+if (srcDir && destDir) {
+	sync(path.resolve(srcDir), path.resolve(destDir), { log: log });
+} else {
+  console.log('Usage: chokdir <srcDir> <destDir>');
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const chokidar = require('chokidar');
+const fs = require('fs-extra');
+
+function copy(srcDir, srcRelativePath, destDir) {
+  const fullSrcPath = path.join(srcDir, srcRelativePath);
+  const fullDestPath = path.join(destDir, srcRelativePath);
+
+  return fs
+  	.ensureDir(path.dirname(fullSrcPath))
+  	.then(() => fs.copy(fullSrcPath, fullDestPath));
+}
+
+function remove(srcDir, srcRelativePath, destDir) {
+	const fullDestPath = path.join(destDir, srcRelativePath);
+	
+	return fs.remove(fullDestPath);
+}
+
+const sync = (srcDir, destDir, opts) => fs.remove(destDir).then(() => {
+  const watcher = chokidar.watch(srcDir, {
+    ignoreInitial: false,
+    ignored: path.join(srcDir, 'node_modules')
+  });
+
+  const log = opts.log || (() => {});
+  const relative = srcFullPath => path.relative(srcDir, srcFullPath);
+
+  watcher.on('add', path => { 
+    copy(srcDir, relative(path), destDir)
+      .then(() => log({ relative: relative(path), type: 'add' }));
+  });
+
+	watcher.on('change', path => { 
+    copy(srcDir, relative(path), destDir)
+      .then(() => log({ relative: relative(path), type: 'change' }));
+  });
+
+	watcher.on('unlink', path => { 
+    remove(srcDir, relative(path), destDir)
+      .then(() => log({ relative: relative(path), type: 'unlink' }));
+  });
+});
+
+module.exports = sync;
